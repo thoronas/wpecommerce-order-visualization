2dc0c1103ea9867f4805abb8ebc33c04cb5a6c7e
Flynn O'Connor <fsquaredesign@gmail.com>
2015-12-04 12:37:53 -0800

    Refactored the pie chart and added titles.

--- admin/assets/js/wcsv.js
+++ admin/assets/js/wcsv.js
@@ -2,6 +2,7 @@
     'use strict';
 
     $(function() {
+		// render_pie_chart( dataset.monthly, "#report" );
 		render_pie_chart( dataset.monthly, "#report" );
 		render_product_sales_graph( dataset.days, "#price-chart" );
 		render_pie_chart( dataset.users, "#users" );
@@ -73,11 +74,11 @@
 
 	}
 
-    function render_pie_chart( dataset, $selector ) {
+    function old_render_pie_chart( dataset, $selector ) {
 
         //Width and height
         var w = 400;
-        var h = 400;
+        var h = 500;
 
         // w should be divided by 2 to make proper pie chart
         var outerRadius = w / 2;
@@ -130,4 +131,137 @@
                 return text;
             });
     }
+
+
+	function render_pie_chart( data, $selector ) {
+
+		var width = 400,
+		    height = 450,
+			radius = Math.min(width, height) / 2;
+		// w should be divided by 2 to make proper pie chart
+        var outerRadius = width;
+		var svg = d3.select( $selector )
+			.append("svg")
+			.append("g")
+			.attr("transform", "translate(" + outerRadius + "," + outerRadius / 2 + ")");
+
+		svg.append("g")
+			.attr("class", "slices");
+		svg.append("g")
+			.attr("class", "labels");
+		svg.append("g")
+			.attr("class", "lines");
+
+		var pie = d3.layout.pie()
+			.value(function(d) {
+				return d.sale_totals;
+			}).sort(d3.ascending);
+		var arc = d3.svg.arc()
+			// .outerRadius(radius * 0.8)
+			// .innerRadius(radius * 0.4);
+			.outerRadius(radius * 0.8)
+			.innerRadius(0);
+		var outerArc = d3.svg.arc()
+			.innerRadius(radius * 0.85)
+			.outerRadius(radius * 0.85);
+
+		// svg.attr("transform", "translate(" + width / 2 + ",500)");
+
+		var key = function(d){ return d.data.name; };
+
+		var color = d3.scale.category20();
+
+		/* ------- PIE SLICES -------*/
+		var slice = svg.select(".slices").selectAll("path.slice")
+			.data(pie(data), key);
+
+		slice.enter()
+			.insert("path")
+			.style("fill", function(d) { return color(d.data.name); })
+			.attr("class", "slice");
+
+		slice
+			.transition().duration(1000)
+			.attrTween("d", function(d) {
+				this._current = this._current || d;
+				var interpolate = d3.interpolate(this._current, d);
+				this._current = interpolate(0);
+				return function(t) {
+					return arc(interpolate(t));
+				};
+			});
+
+		slice.exit()
+			.remove();
+
+		/* ------- TEXT LABELS -------*/
+
+		var text = svg.select(".labels").selectAll("text")
+			.data(pie(data), key);
+
+		text.enter()
+			.append("text")
+			.attr("dy", ".35em")
+			.text(function(d) {
+				return d.data.name + " ($"+d.data.sale_totals+")";
+			})
+			.on("click", function(d){
+				console.log(d);
+			});
+
+		function midAngle(d){
+			return d.startAngle + (d.endAngle - d.startAngle)/2;
+		}
+
+		text.transition().duration(1000)
+			.attrTween("transform", function(d) {
+				this._current = this._current || d;
+				var interpolate = d3.interpolate(this._current, d);
+				this._current = interpolate(0);
+				return function(t) {
+					var d2 = interpolate(t);
+					var pos = outerArc.centroid(d2);
+					pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+					return "translate("+ pos +")";
+				};
+			})
+			.styleTween("text-anchor", function(d){
+				this._current = this._current || d;
+				var interpolate = d3.interpolate(this._current, d);
+				this._current = interpolate(0);
+				return function(t) {
+					var d2 = interpolate(t);
+					return midAngle(d2) < Math.PI ? "start":"end";
+				};
+			});
+
+		text.exit()
+			.remove();
+
+		/* ------- SLICE TO TEXT POLYLINES -------*/
+
+		var polyline = svg.select(".lines").selectAll("polyline")
+			.data(pie(data), key);
+
+		polyline.enter()
+			.append("polyline");
+
+		polyline.transition().duration(1000)
+			.attrTween("points", function(d){
+				this._current = this._current || d;
+				var interpolate = d3.interpolate(this._current, d);
+				this._current = interpolate(0);
+				return function(t) {
+					var d2 = interpolate(t);
+					var pos = outerArc.centroid(d2);
+					pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+					return [arc.centroid(d2), outerArc.centroid(d2), pos];
+				};
+			});
+
+		polyline.exit()
+			.remove();
+
+	}
+
 })(jQuery);
--- admin/orders-display.php
+++ admin/orders-display.php
@@ -19,14 +19,38 @@
   stroke-width: 1.5px;
 }
 
+path.slice{
+	stroke-width:2px;
+}
+
+polyline{
+	opacity: .3;
+	stroke: black;
+	stroke-width: 2px;
+	fill: none;
+}
+.sales-svg.pie svg {
+	height: 400px;
+	width: 100%;
+}
 </style>
 <pre>
 <?php //print_r( wcsv_get_sales_per_category('2015', '11', '2016', '01') ); ?>
 </pre>
 <div class="wrap">
-	<div id="category-sales"></div>
-	<div id="report"></div>
-	<div id="price-chart"></div>
-	<div id="users"></div>
-	<div id="unregistered"></div>
+	<div class="sales-svg pie" id="category-sales">
+		<h2>Sales By Category</h2>
+	</div>
+	<div class="sales-svg pie" id="report">
+		<h2>Top Products</h2>
+	</div>
+	<div class="sales-svg" id="price-chart">
+		<h2>Sales by Month</h2>
+	</div>
+	<div class="sales-svg pie" id="users">
+		<h2>Sales by User</h2>
+	</div>
+	<div class="sales-svg pie" id="unregistered">
+		<h2>Sales by unregistered visitor email</h2>
+	</div>
 </div>
